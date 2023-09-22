20f6ae874892c41e22401c8df3adcd19aee36f3b
AvirukBasak <avirukbasak@gmail.com>
2023-09-22 20:05:31 +0530

    improved the template: checking dependencies

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
 const command = process.argv[2];
+
+global.PATHS = {};
+global.PATHS.build = path.join(__dirname, 'static');
+global.PATHS.app = path.join(__dirname, 'app');
+global.PATHS.server = path.join(__dirname, 'server');
+global.PATHS.appDependencies = path.join(PATHS.app, 'node_modules');
+global.PATHS.serverDependencies = path.join(PATHS.server, 'node_modules');
+
+function testPaths({ install = true, build = true }) {
+    if (install && !fs.existsSync(`${PATHS.appDependencies}`)) {
+        console.error('ERROR: '
+            + `react.js app dependencies at '${PATHS.appDependencies}' not found'\n`
+            + '  run `npm run install` in your project root'
+        );
+        process.exit(1);
+    }
+
+    if (install && !fs.existsSync(`${PATHS.serverDependencies}`)) {
+        console.error('ERROR: '
+            + `express.js server dependencies at '${PATHS.serverDependencies}' not found\n`
+            + '  run `npm run install` in your project root'
+        );
+        process.exit(1);
+    }
+
+    if (build && !fs.existsSync(PATHS.build)) {
+        console.error('ERROR: '
+            + `build path at '${PATHS.build}' not found\n`
+            + '  run `npm run build` in your project root'
+        );
+        process.exit(1);
+    }
+}
 
 function install() {
     execSync('npm install', { cwd: 'app', stdio: 'inherit' });
@@ -7,15 +43,20 @@ function install() {
 }
 
 function build() {
+    testPaths({ install: true, build: false });
     execSync('npm run build', { cwd: 'app', stdio: 'inherit' });
 }
 
 function start() {
-    execSync('npm start', { cwd: 'server', stdio: 'inherit' });
+    testPaths({ install: true, build: true });
+    try {
+        execSync('npm start', { cwd: 'server', stdio: 'inherit' });
+    } catch (e) {}
 }
 
 function dev() {
-    // For now, just call the 'start' script
+    testPaths({ install: true, build: false });
+    execSync('npm run dev', { cwd: 'app', stdio: 'inherit' });
     start();
 }
 
@@ -30,7 +71,6 @@ switch (command) {
         start();
         break;
     case 'dev':
-        build();
         dev();
         break;
     default:
--- server/loadEnv.mjs
+++ server/loadEnv.mjs
@@ -6,15 +6,21 @@ global.__filename = fileURLToPath(import.meta.url);
 global.__dirname = path.dirname(__filename);
 
 global.PATHS = {};
-global.PATHS.public = 'static';
-global.PATHS.build = path.join(__dirname, '..', PATHS.public);
+global.PATHS.build = path.join(__dirname, '..', 'static');
+global.PATHS.app = path.join(__dirname, '..', 'app');
+global.PATHS.server = path.join(__dirname);
+global.PATHS.appDependencies = path.join(PATHS.app, 'node_modules');
+global.PATHS.serverDependencies = path.join(PATHS.server, 'node_modules');
 
 import dotenv from 'dotenv';
 
 dotenv.config();
-dotenv.config({ path: path.resolve(__dirname, '.env.local'), override: true });
+dotenv.config({ path: path.join(__dirname, '.env.local'), override: true });
 
-if (!fs.existsSync(PATHS.build)) throw new Error(
-      `build path '${PATHS.public}' not found in project root\n`
-    + 'run `npm run build` in your project root'
-);
+if (!process.env.MONGODB_URI) {
+    console.error('ERROR: '
+        + 'couldn\'t find MONGODB_URI enviroment variable\n'
+        + `  add MONGODB_URI to '${path.join(__dirname, '.env.local')}'`
+    );
+    process.exit(1);
+}
