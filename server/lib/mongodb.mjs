080d5b20071c12d773b2a6a5451541ed77e7d5ce
AvirukBasak <avirukbasak@gmail.com>
2023-09-27 21:30:31 +0530

    updated mongodb connection module

--- server/lib/mongodb.mjs
+++ server/lib/mongodb.mjs
@@ -6,24 +6,12 @@ const options = {
     useNewUrlParser: true,
 }
 
-let client;
-let connection;
-
 if (!process.env.MONGODB_URI)
     throw new Error('Add MONGODB_URI to .env.local');
 
-if (process.env.NODE_ENV === 'development') {
-    if (!global._mongoConnection) {
-        client = new MongoClient(uri, options);
-        global._mongoConnection = await client.connect();
-    }
-    connection = global._mongoConnection;
-} else {
-    client = new MongoClient(uri, options);
-    connection = await client.connect();
-}
+const client = new MongoClient(uri, options);
+const conn = await client.connect();
 
 console.log(`Connected to MongoDB at '${uri}'`);
 
-const conn = connection;
 export default conn;
